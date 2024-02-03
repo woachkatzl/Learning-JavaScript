2ecb2691e341b23d2a5c85f40a730dac275f4433
woachkatzl <y.v.polonskaya@gmail.com>
2024-02-03 16:06:41 -0500

    getting names of all forms using map

--- form-validation/practice/week16.js
+++ form-validation/practice/week16.js
@@ -4,7 +4,6 @@
 const paragraph = document.getElementById('practicum');
 
 function makeOne() {
-	//Ваш код
 	const allForms = document.forms;
 	const numberOfForms = allForms.length;
 	paragraph.textContent = "Количество форм на странице: " + numberOfForms;
@@ -18,10 +17,23 @@ document.querySelector('.b-1').onclick = makeOne;
 const paragraphTwo = document.getElementById('practicum2');
 
 function makeTwo() {
-	//Ваш код
 	const formOne = document.forms[0];
 	const name = formOne.getAttribute("name");
 	paragraphTwo.textContent = name;
 }
 
 document.querySelector('.b-2').onclick = makeTwo;
+
+//Задание 3
+//Найдите все формы на странице и выведите их названия
+
+const paragraphThree = document.getElementById('practicum3');
+
+function makeThree() {
+	const allForms = document.forms;
+	const formsAsArray = [...allForms];
+	const formNames = formsAsArray.map(form => form.getAttribute("name"));
+	paragraphThree.textContent = formNames.join(", ");
+}
+
+document.querySelector('.b-3').onclick = makeThree;
