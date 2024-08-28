66ae40791d9fa48f32f8386bf7eb97c004198efd
woachkatzl <y.v.polonskaya@gmail.com>
2024-08-28 16:08:52 -0400

    replacing string elements

--- cool-solutions/strings.js
+++ cool-solutions/strings.js
@@ -9,3 +9,31 @@ function getMiddle(s) {
 function getMiddle(s) {
     return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 ? 1 : 2);
 }
+
+//2
+//Return a complimentary side of a DNA, where A and T compliment each other, and G and C. Example: "ATTGC" --> "TAACG"
+
+//My solution:
+function DNAStrand(dna) {
+    const dnaRules = { "A": "T", "T": "A", "C": "G", "G": "C" };
+    return dna.split("").map(letter => dnaRules[letter]).join("");
+}
+
+//Clever solution (is not ideal for long strings, performance would be slower)
+function DNAStrand1(dna) {
+    const dnaRules = { "A": "T", "T": "A", "C": "G", "G": "C" };
+    return dna.replace(/./g, letter => dnaRules[letter]);
+}
+
+//3
+//Turn a DNA string into an RNA string where T becomes U
+
+//My solution:
+function DNAtoRNA(dna) {
+    return dna.replace(/t/gi, 'U');
+}
+
+//Clever solution:
+function DNAtoRNA1(dna) {
+    return dna.split("T"), join("U");
+}
