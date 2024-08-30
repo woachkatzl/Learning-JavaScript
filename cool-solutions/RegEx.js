ebdb7970826f4c1b4076ad38579fa94c85078cda
woachkatzl <y.v.polonskaya@gmail.com>
2024-08-30 15:45:49 -0400

    regex solution

--- cool-solutions/RegEx.js
+++ cool-solutions/RegEx.js
@@ -13,3 +13,13 @@ function abbrevName(name) {
 }
 
 // /\b[a-z]/gi - this expression matches any letter following a non-word character (like a space or the start/end of a string).
+
+//3.
+//Write a function that will return a string showing the number of characters not within a-m range are in the given string compared to the titaol number of characters in the string
+//exapmles: s="aaabbbbhaijjjm" ; printer_error(s) => "0/14"
+//          s="aaaxbbbbyyhwawiwjjjwwm" ; printer_error(s) => "8/22"
+
+function printerError(s) {
+    return `${s.length - s.match(/[a-m]/gi).length}/${s.length}`;
+    //return `${s.match(/[n-z]/gi).length}/${s.length}`;
+}
