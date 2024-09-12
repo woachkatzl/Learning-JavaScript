4acf15806de0f1662323a6631703b4bc7a162a53
woachkatzl <y.v.polonskaya@gmail.com>
2024-09-12 13:46:44 -0400

    CW6 regex, reduce, for in loop

--- code-wars/RegEx.js
+++ code-wars/RegEx.js
@@ -36,3 +36,30 @@ function countSmileys(arr) {
     const smilyesArr = arr.filter(el => /^[:;][~-]?[)D]$/.test(el))
     return smilyesArr.length;
 }
+
+//5.
+//Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+//"abcde" -> 0 # no characters repeats more than once
+//"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+//"indivisibility" -> 1 # 'i' occurs six times
+//"aA11" -> 2 # 'a' and '1'
+
+//My solution
+function duplicateCount(text) {
+    const countObj = text.toLowerCase().split("").reduce((acc, v) => {
+        if (!acc[v]) { acc[v] = 1; }
+        else { acc[v]++; }
+        return acc;
+    }, {}); //return an object with count of each symbol
+    let count = 0;
+    for (let key in countObj) {
+        if (countObj[key] > 1) count++; //if any symbol count is more than 1, increase the output
+    }
+    return count;
+}
+
+//Clever solution
+function duplicateCount1(text) {
+    return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+    //match(/([^])\1+/g) will match any occurance of two or more same consecutive characters like "aa", "bbbb", "555". ([^]) is a capture group that matches any single character. \1 is a backreference to the captured group checking if a character is repeated. + is a quantifier that matches characters reapeated more than once. Without "+" only matches of two cosecutive characters would return like "aa" and "55" not "bbb"
+}
